86b767624ad9af6277156fa2b92a8a759791834b
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2013-03-06 22:22:34 -0500

    More object oriented stuffs and by the way I added the add and list methods

--- lib/guide.rb
+++ lib/guide.rb
@@ -3,16 +3,21 @@
 require File.expand_path('lib/restaurant')
 class Guide
 
+
 	# The initialize method takes an optional argument of the
 	# path to the restaurant file. If user does not supply any
 	# argument we are going to create it	
+	# Am really surprised that the anything in the initialized
+	# that has a print gets called.....DUH! that is why it is an
+	# initialize method. It is supposed to run first when an obj
+	# is created
 	def initialize(path=nil)
 		# locate the restaurant text at the path
 		# Hmmm...you might be wondering why the heck don't we have
 		# an object. well stupid if you remember class methods can be
 		# called on the class
 		Restaurant.filepath = path
-		if Restaurant.file_exists?
+		if Restaurant.file_usable?
 			puts "Found the file"
 		# or create a new file
 		elsif Restaurant.create_file
@@ -24,18 +29,58 @@ class Guide
 		end
 	end
 
-	# Here we define the launch method with ! this means that it
+	# here we define the launch method with ! this means that it
 	# could be destructive, well destructive in the sense that it
 	# launches our app
 	def launch!
 		introduction
-		# action loop
-		#   what do you want to do? (list, find, add, quit)
-		#   do that action
-		# repeat until user quits
+			result = nil
+			until result == :quit
+			# action loop
+			#   what do you want to do? (list, find, add, quit)
+			print "> "
+			user_response = gets.downcase.strip!
+			#   do that action
+			result = do_action(user_response)
+			# repeat until user quits
+		end
 		conclusion
 	end
 	
+	def do_action(action)
+		case action
+		when 'list'
+			list
+		when 'find'
+			puts 'finding'
+		when 'add'
+			addy
+		when 'quit'
+			return :quit
+		else
+			puts "command not valid"
+		end
+	end
+	
+	# command for listing 
+	def list
+		puts "\n Listing restaurants\n\n"
+		restaurants = Restaurant.saved_restaurants
+		restaurants.each do |rest|
+			puts rest.name + " | " + rest.cuisine + " | " + rest.price
+		end
+
+	end
+	# command for add to file	
+	def addy 
+		restaurant = Restaurant.build_using_questions
+		if restaurant.save 
+			puts "\nRestaurant Added\n\n"
+		else
+			puts "\nError: restaurant not added\n\n"
+		end
+	end
+
 	def introduction
 		puts "\n\n<<< Welcome to the Food Finder >>>\n\n"
 		puts "This is an interactive guide to help you find food"		
--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -15,19 +15,83 @@ class Restaurant
 		@@filepath = File.join(APP_ROOT, path)
 	end
 
+	attr_accessor :name, :cuisine, :price
+
 	# Boolean method to check whether the file exists or not
 	def self.file_exists?
 		# class should know that the file a exist	
+		# we are testing to make sure that our variable has
+		# been set and also use ruby's method to check
+		if @@filepath && File.exists?(@@filepath)
+			return true
+		else
+			return false
+		end
+	end
+
+	# Lets try some private class method to use. So it is sort
+	# of basically checking if the file is usable
+	def  self.file_usable?
+		return false unless @@filepath
+		return false unless File.exists?(@@filepath)
+		return false unless File.readable?(@@filepath)
+		return false unless File.writable?(@@filepath)
+		return true
 	end
 
 	# Obviously if the file does not exists we want to create it
 	def self.create_file
-		
+		File.open(@@filepath, 'w') unless file_exists?	
+		return file_usable?
 	end
 
 
 	# Reads the restaurants and then returns us with an instance
 	def self.saved_restaurants
-		
+		restaurants = []
+		if file_usable?
+			file = File.new(@@filepath, 'r')
+			file.each_line do |line|
+				restaurants << Restaurant.new.import_line(line.chomp)
+			end
+			file.close
+		end		
+		return restaurants
+	end
+
+	def import_line(line)
+		line_array = line.split("\t")
+		@name, @cuisine, @price = line_array
+		return self
+	end
+
+	
+	def initialize(args={})
+		@name = args[:name] || ""
+		@cuisine = args[:cuisine] || ""
+		@price =   args[:price] || ""
+	end
+	# save our file to the list
+	def save
+		return false unless Restaurant.file_usable?
+		# All we are doing here is appending some stuffs to end 
+		# of our file
+		File.open(@@filepath, 'a') do |file|
+			file.puts "#{[@name, @cuisine, @price].join("\t")}\n"
+		end
+		return true
+	end
+
+	def self.build_using_questions
+		puts "\n\n << Adding to the menu >>>\n"
+		args = {}
+		print "Restaurant name "
+		args[:name] = gets.chomp.strip
+		print "Cuisine name "
+		args[:cuisine] = gets.chomp.strip
+		print "Price "
+		args[:price] = gets.chomp.strip
+
+		restaurant = Restaurant.new(args)
 	end
 end
